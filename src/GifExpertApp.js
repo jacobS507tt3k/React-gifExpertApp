8fc95e8a1898abad19f73850d98ed5774a02314c
Jacob Sanchez <jacob_ico@outlook.com>
2022-01-21 09:40:37 -0600

    added AddCategory.js and GifGrid.js

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = ()=>{
 
+    const [categories, setCategories] = useState(["Halo"]);
+/*     const handleAdd=()=>{
+        //setCategories(["niño ardilla", ...categories]);
+        setCategories(cats => [...cats, "niño ardilla"] );
+        
+    } */
     return (
 
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
             <hr/>
+
+            <ol>
+                {
+                    categories.map( category => 
+                         <GifGrid 
+                            key={category}
+                            category={category}
+                         />
+                    )
+                }
+            </ol>
 
         </>
 
