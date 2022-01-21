64def71d8961ad574f9bcc0cc34df46210aa73e5
Jacob Sanchez <jacob_ico@outlook.com>
2022-01-21 11:39:12 -0600

    Se agregaron estilos css y modificaciones en AddGrid.js

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
   
-  
-  
+
+    const[images, setImages]=useState([]);
+
+    useEffect(()=>{
+        getGifs();
+    }, [])
+  const getGifs=async()=>{
+      
+      const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=ideOvfFmcBIehKZo7o3GSzJjW2GVsevl`;
+      const resp = await fetch(url);
+      const{data} = await resp.json();
+
+      const gifs = data.map((img)=>{
+            return {
+                id:img.id, 
+                title: img.title,
+                url: img.images?.downsized_medium.url
+
+            }
+      });
+      console.log(gifs);
+      setImages(gifs);
+    }
+
    return( 
   
-   <div>
-       <h3>{category}</h3>
-   </div>
-   
+    <>
+        <h3>{category}</h3>
+        <div className='card-grid'>
+            
+            
+                {
+                    images.map((img)=>(
+                        <GifGridItem 
+                        key={img.id}
+                        { ...img }
+                        />
+                    ))
+                    
+                }
+
+            
+        </div>
+    </>
    
    );
-};
+} ;
